1cf42b8eb5ff9f6aa9b85227d3f259416116dd0e
NachoYuste <ignacioyustel@live.com>
2022-09-16 13:35:12 +0200

    tutorialservice headers

--- TutorialFront/src/app/services/tutorial.service.ts
+++ TutorialFront/src/app/services/tutorial.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Tutorial } from '../models/tutorial';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
 
-  constructor() { }
+  tutorials: Tutorial[] = [];
+  constructor(private http: HttpClient) { }
+
+  getTutorials(): Tutorial[]{
+    
+    this.http.get<Tutorial[]>('http://localhost8080/tutorials').subscribe(data => {
+      this.tutorials = data
+    })
+
+    return this.tutorials;
+  }
+
+  getTutorial(id: number): Tutorial{
+    let tutorial: Tutorial = new Tutorial();
+    
+    this.http.get<Tutorial>('http://localhost8080/tutorials/{{id}}').subscribe(data =>{
+      tutorial = data;
+    });
+    
+    return tutorial;
+  }
+
+  addTutorial(tutorial: Tutorial){
+    const body = {"title":tutorial.title, }
+  }
+
+  editTutorial(tutorial: Tutorial){
+
+  }
+
+  deleteTutorial(tutorial: Tutorial){
+    
+  }
+
+
 }
