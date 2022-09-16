be0939b52e18753fb1c9a54eb15aebdb25d7738d
andrea-sorroche <112932259+andrea-sorroche@users.noreply.github.com>
2022-09-16 13:34:02 +0200

    model, tutorial

--- TutorialFront/src/app/pages/tutorial-desc/tutorial-desc.component.ts
+++ TutorialFront/src/app/pages/tutorial-desc/tutorial-desc.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Tutorial } from 'src/app/models/tutorial';
+import { TutorialService } from 'src/app/services/tutorial.service';
 
 @Component({
   selector: 'app-tutorial-desc',
@@ -7,7 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialDescComponent implements OnInit {
 
-  constructor() { }
+  tutorials: Tutorial[];
+  @Input() tutorial: Tutorial = new Tutorial;
+  tutorialID: number;
+
+  constructor(public tutorialService: TutorialService, route: ActivatedRoute) {
+    this.tutorials = tutorialService.getTutorials();
+    this.tutorialID = parseInt(route.snapshot.paramMap.get('id')!);
+  
+  editTutorial(){
+    this.tutorial = this.tutorialService.editStudent(this.tutorial);
+  }
+
+  deleteTutorial(){
+    this.tutorialService.deleteTutorial(this.tutorial);
+  }
+
 
   ngOnInit(): void {
   }
