2232fe8dc6e3b5e4359ea7d59775f16522cb69a4
NachoYuste <ignacioyustel@live.com>
2022-09-16 12:43:35 +0200

    created angular project and components

---
+++ TutorialFront/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { TutorialListComponent } from './pages/tutorial-list/tutorial-list.component';
+import { AddModalComponent } from './pages/tutorial-list/add-modal/add-modal.component';
+import { TutorialDescComponent } from './pages/tutorial-desc/tutorial-desc.component';
+import { EditModalComponent } from './pages/tutorial-desc/edit-modal/edit-modal.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    TutorialListComponent,
+    AddModalComponent,
+    TutorialDescComponent,
+    EditModalComponent,
+    FooterComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ TutorialFront/src/app/pages/tutorial-desc/tutorial-desc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-desc',
+  templateUrl: './tutorial-desc.component.html',
+  styleUrls: ['./tutorial-desc.component.css']
+})
+export class TutorialDescComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
